b536fae46932fff5605cf4a50745ddec6faf47e7
Tom Schlick <tom@tomschlick.com>
2011-12-06 06:03:32 -0500

    added bootstrap.php file

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * MongoDB CRUD Class
+ *
+ * @package		Mongo CRUD
+ * @link		http://github.com/tomschlick/fuel-mongo-crud
+ *
+ */
+
+Autoloader::add_classes(array(
+	'S3' => __DIR__.'/classes/mongo/crud.php',
+));
